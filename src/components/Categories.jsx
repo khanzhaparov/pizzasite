e1e48078d8b02d20532beeb84a6c6579703dc020
khanzhaparov <khanzhaparov@gmail.com>
2023-04-16 12:32:24 +0600

    4 lesson

---
+++ src/components/Categories.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { useState } from 'react';
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li onClick={() => onClickCategory(index)} className={activeIndex === index ? 'active' : ''}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
